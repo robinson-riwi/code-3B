6925a4bbed949d0ea9b4910fae001ae3306bfa63
Robinson Andres Cortes RIWI <147889972+robinson-riwi@users.noreply.github.com>
2023-11-22 19:28:04 -0500

    test

--- 21-noviembre/js-1/js/app.js
+++ 21-noviembre/js-1/js/app.js
@@ -1,12 +1,14 @@
-
-function operacion(){
-   
-    let operaciones = document.getElementById('operacion').value;
-
-    if(operaciones == 'suma'){
-        console.log("deberias hacer una suma");
-        // numero1 + numero2
+function operaciones(){
+    let operacion = document.getElementById('operacion').value;
+    let numero1 = document.getElementById('numero1').value;
+    let numero2 = document.getElementById('numero2').value;
+    if(operacion!=""){
+        document.getElementById('operacion').classList.add('is-valid');
+        document.getElementById('operacion').classList.remove('is-invalid');
+        document.getElementById('respuesta-2').innerText = "Todo esta muy bien por aqui :)"
+    }else{
+        document.getElementById('operacion').classList.remove('is-valid');
+        document.getElementById('operacion').classList.add('is-invalid');
+        document.getElementById('respuesta-1').innerText = "Todo mal :)"
     }
-   
-
 }
