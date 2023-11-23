336d922881b5d0655b57d181b980332d0c5db4f6
Robinson Andres Cortes RIWI <147889972+robinson-riwi@users.noreply.github.com>
2023-11-23 16:02:28 -0500

    test

--- 21-noviembre/js-2/js/app.js
+++ 21-noviembre/js-2/js/app.js
@@ -1,4 +1,6 @@
+var allPersons = [];
 function guardarUsuario(){
+    
     let nombres = document.getElementById('nombres').value;
     let apellidos = document.getElementById('apellidos').value;
     let correo = document.getElementById('correo').value;
@@ -8,8 +10,15 @@ function guardarUsuario(){
         apellidos : apellidos,
         correo : correo
     }
-    allPersons = [persona];
-    console.log(allPersons);
+    allPersons.push(persona);
+
+    allPersons.forEach(function(person){
+        document.getElementById('resNombre').innerText = person.nombres;
+        document.getElementById('resApellido').innerText = person.apellidos;
+        document.getElementById('resCorreo').innerText = person.correo;
+    })
+    //console.log(allPersons);
+   
 }
 
 
